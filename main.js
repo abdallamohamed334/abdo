6c655204b6d727b18031fa45f045317724cee617
abdallamohamed334 <101903127+abdallamohamed334@users.noreply.github.com>
2022-03-18 13:39:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//consol//
+console.log("hallo");
+console.error("error");
+console.table(["ahmed","abdo"]);
+console.log("hallo js%cjs %cfile","color:red;font-size:40px")
+//ES6//
+/*ECMA script*/
+var myname="ahmed";
+console.log("hallo"+myname);
+//datatype//
+console.log("hallo");
+console.log(typeof"hallo");
+
+console.log(555);
+console.log(typeof 555);
+
+
+
+
+
+
